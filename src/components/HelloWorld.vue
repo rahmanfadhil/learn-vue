bc780c667ec08695f702a82ee42ebd5278475bc2
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-05-18 22:17:31 +0700

    fix bugs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
     <input type="text" v-on:keypress.enter="addItem" v-model="addInput" placeholder="Add Item">
     <button v-on:click="addItem">Add Item</button>
     <br/>
-    <input type="text" v-on:keypress="addItem" v-model="searchInput" placeholder="Search Item">
+    <input type="text" v-model="searchInput" placeholder="Search Item">
     <ul>
       <li v-for="(item, index) in showItem" v-bind:key="index">{{ item }}</li>
     </ul>
